13a166a4bc216893039bb36147f69bbc26291b8a
AmitMardi <amitmardidesigner@gmail.com>
2025-08-02 16:52:31 +0530

    Add files via upload

---
+++ Task_01.py
@@ -0,0 +1,12 @@
+#Task 01: Perform Basic Mathematical Operations
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+# Printing Addition
+print("Addition : ", a + b)
+# Printing Subtraction
+print("Subtraction : ", a - b)
+# Printing Multiplication
+print("Multiplication : ", a * b)
+# Printing Division
+print("Division : ", a / b)
---
+++ Task_02.py
@@ -0,0 +1,6 @@
+#Task 2: Create a Personalized Greeting
+name = input("Ennter Your First Name: ")
+surname = input("Ennter Your Last Name: ")
+
+# Printing Personalized Greeting
+print("👋 Hello, " + name + " " + surname + " ! Welcome to TuteDude 🐍 Python Programming 📚 course.")
